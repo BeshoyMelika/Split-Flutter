cd1dd194a42018334d319827efc781a96417f437
Beshoy Melika <>
2023-07-19 22:28:19 +0300

    move demo data to sperated class

--- lib/feature/expense_details/bloc/expense_details_screen_bloc.dart
+++ lib/feature/expense_details/bloc/expense_details_screen_bloc.dart
@@ -3,18 +3,13 @@ import 'dart:async';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:split/_demo_data/demo_data.dart';
 import 'package:split/feature/expense_details/models/expense_details_ui_model.dart';
-import 'package:split/feature/expense_details/models/payment_details_ui_model.dart';
-import 'package:split/res/app_icons.dart';
-import 'package:split/utils/locale/app_localization_keys.dart';
 part 'expense_details_screen_event.dart';
 part 'expense_details_screen_state.dart';
 
 class ExpenseDetailsScreenBloc
     extends Bloc<ExpenseDetailsScreenEvent, ExpenseDetailsScreenState> {
-  static const networkImageHold =
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4CtaDk3U49ukzwQTk5h6n1mwtWT9HULw-piOF2eF4&s";
-
   ExpenseDetailsScreenBloc() : super(ExpenseDetailsScreenInitial()) {
     on<LoadExpenseDetailsEvent>(_loadExpenseDetailsEvent);
     on<SendReminderToAllUsersExpenseDetailsEvent>(
@@ -27,34 +22,8 @@ class ExpenseDetailsScreenBloc
       Emitter<ExpenseDetailsScreenState> emit) async {
     emit(WaitingExpenseDetailsScreenState());
     await Future.delayed(const Duration(seconds: 1));
-    List<PaymentDetailsUIModel> paymentDetailsList = [
-      PaymentDetailsUIModel(
-          imageURL: networkImageHold,
-          name: "Samuel Adel",
-          amountOfMoney: 220,
-          paymentStatus: paidStatus[PaymentStatus.paid.name] ?? false),
-      PaymentDetailsUIModel(
-          imageURL: networkImageHold,
-          name: "Mina Adly",
-          amountOfMoney: 220,
-          paymentStatus: paidStatus[PaymentStatus.paid.name] ?? false),
-      PaymentDetailsUIModel(
-          imageURL: networkImageHold,
-          name: "Amgad Nady",
-          amountOfMoney: 220,
-          paymentStatus: paidStatus[PaymentStatus.notYet.name] ?? false),
-    ];
-    ExpenseDetailsUIModel expenseDetails = ExpenseDetailsUIModel(
-        expenseTitle: LocalizationKeys.food,
-        expenseIcon: AppIcons.food,
-        amountSpent: 520,
-        lastUpdate: DateTime.now(),
-        dueDateForPay: DateTime.now(),
-        amountReceivable: 220,
-        paidBy: "Besho",
-        paidByImageURL: networkImageHold,
-        splitUpon: paymentDetailsList);
-    emit(LoadedExpenseDetailsSuccessfullyState(expenseDetails: expenseDetails));
+    emit(LoadedExpenseDetailsSuccessfullyState(
+        expenseDetails: DemoData.expenseDetails));
   }
 
   FutureOr<void> _sendReminderToAllUsersExpenseDetailsEvent(
@@ -65,7 +34,3 @@ class ExpenseDetailsScreenBloc
       AddPhotoOrReceiptForExpenseDetailsEvent event,
       Emitter<ExpenseDetailsScreenState> emit) {}
 }
-
-const paidStatus = {"paid": true, "notYet": false};
-
-enum PaymentStatus { paid, notYet }
